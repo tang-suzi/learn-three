a2191b65ab5c944e9e1b118d2866539d1254dfee
Baron Tang <tangtang960225@163.com>
2025-03-02 11:48:57 +0800

    feat: 添加坐标辅助线

--- src/views/AddAxesHelper.tsx
+++ src/views/AddAxesHelper.tsx
@@ -1,8 +1,8 @@
 import { FC, useEffect, useRef } from "react";
 import * as THREE from "three";
 
-const HelloThree: FC = () => {
-  const threeDemo = useRef<HTMLDivElement>(null);
+const AddAxesHelper: FC = () => {
+  const addAxesHelperRef = useRef<HTMLDivElement>(null);
   const hasInit = useRef(false);
   const init = () => {
     if (hasInit.current) return;
@@ -10,16 +10,23 @@ const HelloThree: FC = () => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
     camera.position.z = 5;
+    camera.position.y = 2;
+    camera.position.x = 1;
+    camera.lookAt(0, 0, 0);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(
-      threeDemo.current?.clientWidth,
-      threeDemo.current?.clientHeight
+      addAxesHelperRef.current?.clientWidth,
+      addAxesHelperRef.current?.clientHeight
     );
-    threeDemo.current?.appendChild(renderer.domElement);
+    addAxesHelperRef.current?.appendChild(renderer.domElement);
+
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+    // 世界坐标辅助器
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
     const animate = () => {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
@@ -32,10 +39,11 @@ const HelloThree: FC = () => {
     init();
   }, []);
   return (
-    <>
-      <div ref={threeDemo} style={{ width: "400px", height: "400px" }}></div>
-    </>
+    <div
+      ref={addAxesHelperRef}
+      style={{ width: "400px", height: "400px" }}
+    ></div>
   );
 };
 
-export default HelloThree;
+export default AddAxesHelper;
