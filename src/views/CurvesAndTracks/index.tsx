810067d2c8ed57dca6a29d9d390944411bec8656
Baron Tang <tangtang960225@163.com>
2025-05-11 17:12:20 +0800

    feat: 让小球根据轨迹移动

--- src/views/CurvesAndTracks/index.tsx
+++ src/views/CurvesAndTracks/index.tsx
@@ -9,7 +9,10 @@ const CurvesAndTracks: FC = () => {
   let camera: THREE.PerspectiveCamera | null = null;
   let renderer: THREE.WebGLRenderer | null = null;
   let controls: OrbitControls | null = null;
-
+  let moonMesh: THREE.Mesh | null = null;
+  let curve: THREE.CatmullRomCurve3 | null = null;
+  const clock = new THREE.Clock();
+  const textureLoader = new THREE.TextureLoader();
   const init = () => {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(
@@ -33,13 +36,22 @@ const CurvesAndTracks: FC = () => {
     scene.add(axesHelper);
   };
   const animate = () => {
+    const elapsed = clock.getElapsedTime();
+    if (moonMesh && curve) {
+      //   const points = curve.getPoints(50);
+      //   const pointIndex = Math.floor(((elapsed % 5) / 5) * points.length); // 计算当前点位索引
+      //   moonMesh.position.copy(points[pointIndex]); // 更新小球位置
+      const time = (elapsed * 0.1) % 1; // 计算当前时间
+      const point = curve.getPointAt(time);
+      moonMesh.position.copy(point); // 更新小球位置
+    }
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
   };
   const createObjects = () => {
     // 根据点位创建曲线
-    const curve = new THREE.CatmullRomCurve3(
+    curve = new THREE.CatmullRomCurve3(
       [
         new THREE.Vector3(-2, 0, 2),
         new THREE.Vector3(-1, 1, 1),
@@ -59,11 +71,31 @@ const CurvesAndTracks: FC = () => {
 
     const curveObject = new THREE.Line(geometry, material);
     scene.add(curveObject);
+
+    const moonGeometry = new THREE.SphereGeometry(0.27, 16, 16);
+    const moonMaterial = new THREE.MeshPhongMaterial({
+      shininess: 5,
+      map: textureLoader.load(
+        new URL("./../../assets/texture/planets/moon_1024.jpg", import.meta.url)
+          .href
+      ),
+    });
+    moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+    scene.add(moonMesh);
+  };
+  const createLight = () => {
+    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight.position.set(0, 0, 1);
+    dirLight.layers.enableAll();
+    scene.add(dirLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
   };
   useEffect(() => {
     if (!hasInit.current) {
       hasInit.current = true;
       init();
+      createLight();
       createObjects();
       animate();
     }
