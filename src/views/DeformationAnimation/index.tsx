a43c1a032e233e8b2a11ed31dbd1c0d51461b684
Baron Tang <tangtang960225@163.com>
2025-05-12 11:16:14 +0800

    feat: 实现物体变形，实现花朵生长及花开效果

--- src/views/DeformationAnimation/index.tsx
+++ src/views/DeformationAnimation/index.tsx
@@ -56,37 +56,134 @@ const DeformationAnimation: FC = () => {
       }
     );
   };
-  const loadGltf = () => {
-    gltfLoader.load(
-      new URL("./../../assets/model/sphere1.glb", import.meta.url).href,
-      (gltf) => {
-        const model = gltf.scene;
-        console.log(model.children[0]);
-        scene.add(model);
-        const sphere1 = model.children[0];
-        gltfLoader.load(
-          new URL("./../../assets/model/sphere2.glb", import.meta.url).href,
-          (gltf1) => {
-            sphere1.geometry.morphAttributes.position = [];
-            sphere1.geometry.morphAttributes.position.push(
-              gltf1.scene.children[0].geometry.attributes.position
-            );
-            sphere1.updateMorphTargets();
-            sphere1.morphTargetInfluences[0] = 1;
-            const params = { value: 0 };
-            gsap.to(params, {
-              value: 1,
-              duration: 2,
-              repeat: -1,
-              yoyo: true,
-              onUpdate: () => {
-                sphere1.morphTargetInfluences[0] = params.value;
-              },
-            });
-          }
-        );
-      }
+  const loadGltf = async () => {
+    // gltfLoader.load(
+    //   new URL("./../../assets/model/sphere1.glb", import.meta.url).href,
+    //   (gltf) => {
+    //     const model = gltf.scene;
+    //     scene.add(model);
+    //     const sphere1 = model.children[0];
+    //     gltfLoader.load(
+    //       new URL("./../../assets/model/sphere2.glb", import.meta.url).href,
+    //       (gltf1) => {
+    //         sphere1.geometry.morphAttributes.position = [];
+    //         sphere1.geometry.morphAttributes.position.push(
+    //           gltf1.scene.children[0].geometry.attributes.position
+    //         );
+    //         sphere1.updateMorphTargets();
+    //         sphere1.morphTargetInfluences[0] = 1;
+    //         const params = { value: 0 };
+    //         gsap.to(params, {
+    //           value: 1,
+    //           duration: 2,
+    //           repeat: -1,
+    //           yoyo: true,
+    //           onUpdate: () => {
+    //             sphere1.morphTargetInfluences[0] = params.value;
+    //           },
+    //         });
+    //       }
+    //     );
+    //   }
+    // );
+    const params = { value0: 0, value1: 0 };
+    let petal: THREE.Mesh = null;
+    let petal1 = null;
+    let petal2 = null;
+    let stem: THREE.Mesh = null;
+    let stem1 = null;
+    let stem2 = null;
+    const { scene: f4Scene } = await gltfLoader.loadAsync(
+      new URL("./../../assets/model/f4.glb", import.meta.url).href
     );
+    f4Scene.rotation.x = Math.PI;
+    f4Scene.traverse((item) => {
+      if (item.material && item.material.name === "Water") {
+        item.material = new THREE.MeshStandardMaterial({
+          color: "skyblue",
+          depthWrite: false,
+          depthTest: false,
+          transparent: true,
+          opacity: 0.7,
+        });
+      }
+      if (item.material && item.material.name === "Stem") {
+        stem = item;
+      }
+      if (item.material && item.material.name === "Petal") {
+        petal = item;
+      }
+    });
+    scene.add(f4Scene);
+    const { scene: f2Scene } = await gltfLoader.loadAsync(
+      new URL("./../../assets/model/f2.glb", import.meta.url).href
+    );
+    f2Scene.traverse((item) => {
+      if (item.material && item.material.name === "Petal") {
+        petal1 = item;
+        if (!petal.geometry.morphAttributes.position) {
+          petal.geometry.morphAttributes.position = [];
+        }
+        petal.geometry.morphAttributes.position.push(
+          petal1.geometry.attributes.position
+        );
+
+        petal.updateMorphTargets();
+        petal.morphTargetInfluences[0] = 1;
+      }
+      if (item.material && item.material.name === "Stem") {
+        stem1 = item;
+        if (!stem.geometry.morphAttributes.position) {
+          stem.geometry.morphAttributes.position = [];
+        }
+        stem.geometry.morphAttributes.position.push(
+          stem1.geometry.attributes.position
+        );
+        stem.updateMorphTargets();
+        stem.morphTargetInfluences[0] = 1;
+      }
+      gsap.to(params, {
+        value0: 1,
+        duration: 4,
+        delay: 0,
+        onUpdate: () => {
+          petal.morphTargetInfluences[0] = params.value0;
+          stem.morphTargetInfluences[0] = params.value0;
+        },
+      });
+    });
+    const { scene: f1Scene } = await gltfLoader.loadAsync(
+      new URL("./../../assets/model/f1.glb", import.meta.url).href
+    );
+    f1Scene.traverse((item) => {
+      if (item.material && item.material.name === "Petal") {
+        petal2 = item;
+        petal.geometry.morphAttributes.position.push(
+          petal2.geometry.attributes.position
+        );
+        petal.updateMorphTargets();
+        petal.morphTargetInfluences[1] = 0;
+      }
+      if (item.material && item.material.name === "Stem") {
+        stem2 = item;
+        stem.geometry.morphAttributes.position.push(
+          stem2.geometry.attributes.position
+        );
+        stem.updateMorphTargets();
+        stem.morphTargetInfluences[1] = 0;
+      }
+      gsap.to(params, {
+        value1: 1,
+        duration: 4,
+        delay: 3,
+        onUpdate: function () {
+          petal.morphTargetInfluences[0] = params.value0;
+          stem.morphTargetInfluences[0] = params.value0;
+          petal.morphTargetInfluences[1] = params.value1;
+          stem.morphTargetInfluences[1] = params.value1;
+        },
+      });
+    });
   };
   const animate = () => {
     requestAnimationFrame(animate);
